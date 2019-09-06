4b80d2307dd73328fd33e67a0a795923e6c4938f
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-06 11:21:12 -0400

    completes header.js, header displays properly

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,11 +3,14 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+
+const headContainer = document.querySelector('.header-container')
+headContainer.appendChild(Header());
 
 function Header() {
     //create elements according to html tree
@@ -21,5 +24,16 @@ function Header() {
     newDate.classList.add('date');
     newTemp.classList.add('temp');
 
+    //add content to elements
+    newDate.textContent = 'March 28, 2019';
+    newTitle.textContent = 'Lambda Times';
+    newTemp.textContent = '98°';
 
+    //append children to newHeader element
+    newHeader.appendChild(newDate);
+    newHeader.appendChild(newTitle);
+    newHeader.appendChild(newTemp);
+
+
+    return newHeader;
 }
